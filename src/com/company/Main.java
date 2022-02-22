a1eaa1d626eed1c69a7eb082f18a4d7e48466375
Brennast26 <brennast@mail.uc.edu>
2022-02-21 19:39:49 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	String partyaffiliation = "A";
+    if (partyaffiliation == "D")
+        {
+            System.out.println("You get a Democratic Donkey");
+        }
+    else if (partyaffiliation == "R")
+        {
+         System.out.println("You get a Republican Elephant");
+        }
+    else if (partyaffiliation == "I")
+        {
+        System.out.println("You get an Independant Person.");
+        }
+    else
+        {
+        System.out.println("You get other.");
+        }
+    }
+}
